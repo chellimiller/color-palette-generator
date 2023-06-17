0b0ccc9bc4fc0fd09ccae0167d957b345b643f2a
Michelle Miller <chellimiller@users.noreply.github.com>
2023-06-17 15:04:25 -0700

    Remove popover

--- src/ui/Popover/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './PopoverContent';
-export * from './PopoverProvider';
-export * from './PopoverTrigger';
-export { default as PopoverContent } from './PopoverContent';
-export { default as PopoverProvider } from './PopoverProvider';
-export { default as PopoverTrigger } from './PopoverTrigger';
