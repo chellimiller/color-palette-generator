3aae1dacceb6ca0ad1592b887eea4a15c49e64d7
Michelle Miller <chellimiller@users.noreply.github.com>
2023-06-17 15:04:11 -0700

    Add popover

---
+++ src/ui/Popover/PopoverTrigger.tsx
@@ -0,0 +1,42 @@
+import { useMergeRefs } from '@floating-ui/react';
+import React from 'react';
+import { usePopoverContext } from './PopoverProvider';
+
+interface PopoverTriggerProps {
+  children: React.ReactNode;
+  asChild?: boolean;
+}
+
+const PopoverTrigger = React.forwardRef<
+  HTMLElement,
+  React.HTMLProps<HTMLElement> & PopoverTriggerProps
+>(function PopoverTrigger({ children, asChild = false, ...props }, propRef) {
+  const context = usePopoverContext();
+  const childrenRef = (children as any).ref;
+  const ref = useMergeRefs([context.refs.setReference, propRef, childrenRef]);
+
+  if (asChild && React.isValidElement(children)) {
+    return React.cloneElement(
+      children,
+      context.getReferenceProps({
+        ref,
+        ...props,
+        ...children.props,
+        'data-state': context.open ? 'open' : 'closed',
+      })
+    );
+  }
+
+  return (
+    <button
+      ref={ref}
+      type="button"
+      data-state={context.open ? 'open' : 'closed'}
+      {...context.getReferenceProps(props)}
+    >
+      {children}
+    </button>
+  );
+});
+
+export default PopoverTrigger;
